dd77e4fbef62af2cbf70e8e56caa338e5178f8e4
David Ragel <daradija@gmail.com>
2023-06-24 16:45:24 +0200

    razones trigonometricas

--- README.md
+++ README.md
@@ -16,8 +16,37 @@ hipotenusa=math.sqrt(cateto_contiguo**2+cateto_opuesto**2)
 print("Hipotenusa: ", hipotenusa)
 ```
 
-````
+```
 Cateto contiguo:  59.8
 Cateto opuesto:  33.6
 Hipotenusa:  68.59300255857006
 ```
+
+![Ejemplo con turtle](imagenes/turtle.png)
+```python
+import turtle
+turtle.penup()
+turtle.goto(-cateto_contiguo/2,0)
+turtle.pendown()
+
+turtle.forward(cateto_contiguo)
+turtle.left(90)
+turtle.forward(cateto_opuesto)
+turtle.left(90)
+turtle.forward(cateto_contiguo)
+turtle.left(90)
+turtle.forward(cateto_opuesto)
+turtle.left(90)
+
+angulo=math.atan(cateto_opuesto/cateto_contiguo)
+# radianes a grados
+angulo=angulo*180/math.pi
+turtle.left(angulo)
+turtle.forward(hipotenusa)
+
+turtle.done()
+# pause until press a key
+input("Press any key to finish")
+
+
+
--- ratry.py
+++ ratry.py
@@ -1,22 +1,34 @@
 import math
 
 # Ejemplo de Razones trigonométricas
-cateto_contiguo=59.8
-cateto_opuesto=33.6
+cateto_contiguo=598
+cateto_opuesto=336
 print("Cateto contiguo: ", cateto_contiguo)
 print("Cateto opuesto: ", cateto_opuesto)
 hipotenusa=math.sqrt(cateto_contiguo**2+cateto_opuesto**2)
 print("Hipotenusa: ", hipotenusa)
 
-# Datos
-angulo = 30
-hipotenusa = 10
 
-print("Angulo: ", angulo)
-print("Hipotenusa: ", hipotenusa)
+import turtle
+turtle.penup()
+turtle.goto(-cateto_contiguo/2,0)
+turtle.pendown()
 
-# Cálculos
-cateto_opuesto = hipotenusa * math.sin(math.radians(angulo))
-cateto_contiguo = hipotenusa * math.cos(math.radians(angulo))
-print("Cateto opuesto: ", cateto_opuesto)
-print("Cateto contiguo: ", cateto_contiguo)
+turtle.forward(cateto_contiguo)
+turtle.left(90)
+turtle.forward(cateto_opuesto)
+turtle.left(90)
+turtle.forward(cateto_contiguo)
+turtle.left(90)
+turtle.forward(cateto_opuesto)
+turtle.left(90)
+
+angulo=math.atan(cateto_opuesto/cateto_contiguo)
+# radianes a grados
+angulo=angulo*180/math.pi
+turtle.left(angulo)
+turtle.forward(hipotenusa)
+
+turtle.done()
+# pause until press a key
+input("Press any key to finish")
